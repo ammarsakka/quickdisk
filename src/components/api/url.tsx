775acfa2646a4295b72439ca7b5f4f3491153539
Ammar Sakka <117344965+ammarsakka@users.noreply.github.com>
2023-04-27 15:39:48 +0800

    main

--- src/components/api/url.tsx
+++ src/components/api/url.tsx
@@ -1,2 +1,2 @@
-export const API_URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`
-// export const API_URL = `${process.env.REACT_APP_SERVER_URL}`
+// export const API_URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`
+export const API_URL = `${process.env.REACT_APP_SERVER_URL}`
