f4e7c2c2722ac68fa1566720f11aa5b195ae2344
Ammar Sakka <117344965+ammarsakka@users.noreply.github.com>
2023-06-14 16:01:02 +0800

    Update url.tsx

--- src/components/api/url.tsx
+++ src/components/api/url.tsx
@@ -1,2 +1,2 @@
 // export const API_URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`
-export const API_URL = `${process.env.REACT_APP_SERVER_URL}`
+export const API_URL = "https://5c2a-115-133-72-36.ngrok-free.app"
